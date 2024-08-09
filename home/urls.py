eebc06f691c1e67f939c5214409bae02c4ded05f
Abhi <abhijoshi1441@gmail.com>
2024-08-09 19:28:34 +1000

    Configured Multiple Functionalities
    -Made search and side bars functional
    -Designed Shop and Deals Landing Pages
    -Divided internal structure into separate components

--- home/urls.py
+++ home/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
 	path('login/', views.loginUser, name="login"),
 	path('logout/', views.logoutUser, name="logout"),
 	path('register/', views.registerUser, name="register"),
-	path('shop/', views.shop, name="shop"),
-	path('product/<str:pk>', views.product, name="product"),
-	path('cart/', views.cart, name="cart"),
-	path('learn/', views.learn, name="learn"),
+	path('shop/', views.shopPage, name="shop"),
+	path('product/<str:pk>', views.productDisplay, name="product"),
+	path('cart/', views.cartPage, name="cart"),
+	path('learn/', views.learnPage, name="learn"),
+	path('deals/', views.dealsPage, name="deals"),
 ]
--- home/views.py
+++ home/views.py
@@ -62,23 +62,47 @@ def registerUser(request):
     context = {'form':form}
     return render(request, 'login_register.html', context)
 
-def shop(request):
+def shopPage(request):
+    page = 'shop'
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    
+    products = productItem.objects.filter(
+        Q(subcategory__category__name__icontains = q) |
+        Q(subcategory__name__icontains = q) |
+        Q(name__icontains = q)
+    )
+    
     categories = productCategory.objects.all()
     subcategories = productSubcategory.objects.all()
-    products = productItem.objects.all()
-    onDealProducts = productItem.objects.filter(onDeal=True)
-    context = {'categories':categories, 'subcategories':subcategories, 'products':products, 'onDealProducts':onDealProducts}
+    product_count = products.count()
+    #onDealProducts = productItem.objects.filter(onDeal=True)
+    context = {'page':page, 'categories':categories, 'subcategories':subcategories, 'products':products, 'product_count':product_count}
     return render(request, 'shop.html', context)
 
-def product(request, pk):
+def productDisplay(request, pk):
     context = {}
-    return render(request, 'product.html', context)
+    return render(request, 'product_display.html', context)
 
 @login_required(login_url='login')
-def cart(request):
+def cartPage(request):
     context = {}
     return render(request, 'cart.html', context)
 
-def learn(request):
+def learnPage(request):
     context = {}
     return render(request, 'learn.html', context)
+
+def dealsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    
+    onDealProducts = productItem.objects.filter(onDeal=True)
+    products = onDealProducts.filter(
+        Q(subcategory__name__icontains = q) |
+        Q(name__icontains = q)
+    )
+    
+    categories = productCategory.objects.all()
+    subcategories = productSubcategory.objects.all()
+    product_count = products.count()
+    context = {'categories':categories, 'subcategories':subcategories, 'products':products, 'product_count':product_count}
+    return render(request, 'deals.html', context)
